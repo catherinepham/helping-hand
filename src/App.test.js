1ad7a731457401397c1c8e3e3189dcea3031a8ac
Catherine Pham <73704202+catherinepham@users.noreply.github.com>
2022-03-26 12:27:25 -0400

    Delete src directory

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-const { declareTypeAlias } = require('@babel/types')
-const App = require('./App')
-
-/*test('checks successful login') {
-    details.email = "admin@admin.com"
-    details.password = "admin"
-};*/
