56bcb18a0c3eb31ea23726123f666cdef3890ca1
catherinepham <cathy.02pham@gmail.com>
2022-04-15 13:25:03 -0400

    Adding backend connection to mongodb

---
+++ server/routes/student.js
@@ -0,0 +1,84 @@
+const express = require("express");
+
+// studentRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /student.
+const studentRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+
+// This section will help you get a list of all the students.
+studentRoutes.route("/student").get(function (req, res) {
+  let db_connect = dbo.getDb("teachers");
+  db_connect
+    .collection("students")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single student by id
+studentRoutes.route("/student/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect
+      .collection("students")
+      .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+});
+
+// This section will help you create a new student.
+studentRoutes.route("/student/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    name: req.body.name,
+    classcode: req.body.classcode,
+    teacher: req.body.teacher,
+  };
+  db_connect.collection("students").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a student by id.
+studentRoutes.route("/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  let newvalues = {
+    $set: {
+      name: req.body.name,
+      classcode: req.body.classcode,
+      teacher: req.body.teacher,
+    },
+  };
+  db_connect
+    .collection("students")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+// This section will help you delete a student
+studentRoutes.route("/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect.collection("students").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+module.exports = studentRoutes;
